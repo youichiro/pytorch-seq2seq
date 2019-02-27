e4f8e163f4ee752872959b9ca6c891c3add9d0ce
youichiro <cinnamon416@gmail.com>
2019-02-27 15:06:04 +0900

    first commit

---
+++ nets.py
@@ -0,0 +1,126 @@
+#-*- coding: utf-8 -*-
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import random
+
+
+class RNNEncoder(nn.Module):
+    def __init__(self, n_vocab, n_emb, n_unit, n_layer, dropout):
+        super().__init__()
+        self.embedding = nn.Embedding(n_vocab, n_emb)
+        self.rnn = nn.LSTM(n_emb, n_unit, n_layer)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, src_seqs):
+        embedded = self.dropout(self.embedding(src_seqs))
+        enc_outs, hs = self.rnn(embedded)
+        return enc_outs, hs
+
+
+class RNNDecoder(nn.Module):
+    def __init__(self, n_vocab, n_emb, n_unit, n_layer, dropout):
+        super().__init__()
+        self.embedding = nn.Embedding(n_vocab, n_emb)
+        self.rnn = nn.LSTM(n_emb, n_unit, n_layer)
+        self.wo = nn.Linear(n_unit, n_vocab)
+        self.dropout = nn.Dropout(dropout)
+        self.n_vocab = n_vocab
+
+    def forward(self, tgt_words, hs):
+        tgt_words = tgt_words.unsqueeze(0)  # 次元を一つ上げる
+        embedded = self.dropout(self.embedding(tgt_words))
+        dec_outs, hs = self.rnn(embedded, hs)
+        pred = self.wo(dec_outs.squeeze(0))  # 次元を一つ下げる
+        return pred, hs
+
+
+class Attention(nn.Module):
+    def __init__(self, method, n_unit):
+        super().__init__()
+        self.method = method
+        self.n_unit = n_unit
+        if self.method not in ['dot', 'general', 'concat']:
+            raise ValueError(self.method, "Is not an appropriate attention method.")
+        if method == 'general':
+            self.w = nn.Linear(n_unit, n_unit)
+        elif method == 'concat':
+            self.w = nn.Linear(n_unit * 2, n_unit)
+            self.v = torch.nn.Parameter(torch.FloatTensor(n_unit))
+
+    def forward(self, dec_out, enc_outs):
+        if self.method == 'dot':
+            attn_energies = self.dot_score(dec_out, enc_outs)
+        elif self.method == 'general':
+            attn_energies = self.general_score(dec_out, enc_outs)
+        elif self.method == 'concat':
+            attn_energies = self.concat_score(dec_out, enc_outs)
+        return F.softmax(attn_energies, dim=0)
+
+    def dot_score(self, dec_out, enc_outs):
+        return torch.sum(dec_out * enc_outs, dim=2)
+
+    def general_score(self, dec_out, enc_outs):
+        energy = self.w(enc_outs)
+        return torch.sum(dec_out * energy, dim=2)
+
+    def concat_score(self, dec_out, enc_outs):
+        dec_out = dec_out.expand(enc_outs.shape[0], -1, -1)
+        energy = torch.cat((dec_out, enc_outs), 2)
+        return torch.sum(self.v * self.w(energy).tanh(), dim=2)
+
+
+class RNNAttnDecoder(nn.Module):
+    def __init__(self, n_vocab, n_emb, n_unit, n_layer, dropout, attn):
+        super().__init__()
+        self.embedding = nn.Embedding(n_vocab, n_emb)
+        self.rnn = nn.LSTM(n_emb, n_unit, n_layer)
+        self.wc = nn.Linear(n_unit * 2, n_unit)
+        self.attn = Attention(attn, n_unit)
+        self.wo = nn.Linear(n_unit, n_vocab)
+        self.dropout = nn.Dropout(dropout)
+        self.n_vocab = n_vocab
+
+    def forward(self, tgt_words, hs, enc_outs):
+        tgt_words = tgt_words.unsqueeze(0)
+        embedded = self.dropout(self.embedding(tgt_words))
+        dec_out, hs = self.rnn(embedded, hs)
+        attn_weights = self.attn(dec_out, enc_outs)
+        context = torch.bmm(attn_weights.transpose(1, 0).unsqueeze(1),
+                            enc_outs.transpose(1, 0)
+                  ).transpose(1, 0)
+        cats = self.wc(torch.cat((dec_out, context), dim=2)).tanh()
+        pred = self.wo(cats.squeeze(0))
+        return pred, hs
+
+
+class Seq2seq(nn.Module):
+    def __init__(self, encoder, decoder, sos_id, device):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.sos_id = sos_id
+        self.device = device
+
+    def forward(self, src_seqs, tgt_seqs, maxlen, teaching_force_ratio=0.5):
+        bs = src_seqs.shape[1]
+        enc_outs, hs = self.encoder(src_seqs)
+
+        if tgt_seqs is None:  # testing mode
+            inputs = torch.ones(bs, dtype=torch.int64) * self.sos_id
+            inputs = inputs.to(self.device)
+        else:  # training mode
+            maxlen = tgt_seqs.shape[0]
+            inputs = tgt_seqs[0]
+
+        outputs = torch.zeros((maxlen, bs, self.decoder.n_vocab))
+        outputs = outputs.to(self.device)
+        for i in range(1, maxlen):
+            preds, hs = self.decoder(inputs, hs, enc_outs)
+            outputs[i] = preds
+            teaching_force = random.random() < teaching_force_ratio
+            top1 = preds.max(1)[1]
+            inputs = (tgt_seqs[i] if teaching_force else top1)
+        return outputs
---
+++ train.py
@@ -0,0 +1,141 @@
+#-*- coding: utf-8 -*-
+
+import math
+import os
+import argparse
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchtext.data import Field, TabularDataset, BucketIterator
+
+from nets import RNNEncoder, RNNAttnDecoder, Seq2seq
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def main():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--train', required=True, help='TSV file for training (.tsv)')
+    parser.add_argument('--valid', required=True, help='TSV file for validation (.tsv)')
+    parser.add_argument('--save-dir', required=True, help='Save directory')
+    parser.add_argument('--model', required=True, help='Model name (.pt)')
+    parser.add_argument('--attn', choices=['dot', 'general', 'concat'], default='dot',
+                        help='Select attention method')
+    parser.add_argument('--batchsize', type=int, default=64, help='Batch size')
+    parser.add_argument('--embsize', type=int, default=128, help='Embedding size')
+    parser.add_argument('--unit', type=int, default=128, help='Number of unit')
+    parser.add_argument('--layer', type=int, default=2, help='Number of layer')
+    parser.add_argument('--dropout', type=float, default=0.2, help='Dropout rate')
+    parser.add_argument('--clip', type=float, default=10, help='Clipping gradients')
+    parser.add_argument('--epoch', type=int, default=25, help='Max epoch')
+    args = parser.parse_args()
+
+    # setup data
+    SRC = Field(init_token='<sos>', eos_token='<eos>', lower=True)
+    TRG = Field(init_token='<sos>', eos_token='<eos>', lower=True)
+
+    train_data, valid_data = TabularDataset.splits(
+        path='.',
+        train=args.train,
+        validation=args.valid,
+        format='tsv',
+        fields=[('src', SRC), ('trg', TRG)]
+    )
+
+    print(f'# training examples: {len(train_data)}')
+    print(f'# validation examples: {len(valid_data)}')
+    print('')
+
+    SRC.build_vocab(train_data, min_freq=2)
+    TRG.build_vocab(train_data, min_freq=2)
+
+    vocabs = {'src_stoi': SRC.vocab.stoi, 'src_itos': SRC.vocab.itos,
+              'trg_stoi': TRG.vocab.stoi, 'trg_itos': TRG.vocab.itos}
+
+    print(f'Unique tokens in source vocabulary: {len(SRC.vocab)}')
+    print(f'Unique tokens in target vocabulary: {len(TRG.vocab)}')
+    print('')
+
+    train_iter, valid_iter = BucketIterator.splits(
+        (train_data, valid_data),
+        batch_size=args.batchsize,
+        sort_within_batch=True,
+        sort_key=lambda x: len(x.src),
+        repeat=False,
+        device=device
+    )
+
+    # setup model
+    sos_id = TRG.vocab.stoi['<sos>']
+    encoder = RNNEncoder(len(SRC.vocab), args.embsize, args.unit,
+                         args.layer, args.dropout)
+    decoder = RNNAttnDecoder(len(TRG.vocab), args.embsize, args.unit,
+                             args.layer, args.dropout, args.attn)
+    model = Seq2seq(encoder, decoder, sos_id, device).to(device)
+    params = {'n_emb': args.embsize, 'n_unit': args.unit, 'n_layer': args.layer,
+              'dropout': args.dropout, 'attn': args.attn}
+    print(model)
+    print()
+
+    optimizer = optim.Adam(model.parameters())
+    criterion = nn.CrossEntropyLoss(ignore_index=TRG.vocab.stoi['<pad>'])
+
+    # make directory for saving model
+    if not os.path.isdir(args.save_dir):
+        os.mkdir(args.save_dir)
+    model_path = os.path.join(args.save_dir, args.model)
+
+    # training and validation
+    best_loss = float('inf')
+    for epoch in range(args.epoch):
+        train_loss = train(model, train_iter, optimizer, criterion, args.clip)
+        valid_loss = eval(model, valid_iter, criterion)
+
+        if valid_loss < best_loss:
+            best_loss = valid_loss
+            state = {'vocabs': vocabs, 'params': params,
+                     'state_dict': model.state_dict()}
+            torch.save(state, model_path)
+
+        print(f' | Epoch: {epoch+1:03}', end='')
+        print(f' | Train loss: {train_loss:.3f}', end='')
+        print(f' | Train PPL: {math.exp(train_loss):7.3f}', end='')
+        print(f' | Val. Loss {valid_loss:.3f}', end='')
+        print(f' | Val. PPL: {math.exp(valid_loss): 7.3f} |')
+
+
+def train(model, iterator, optimizer, criterion, clip):
+    model.train()
+    epoch_loss = 0
+
+    for batch in iterator:
+        src = batch.src
+        trg = batch.trg
+        optimizer.zero_grad()
+        outs = model(src, trg, None)
+        loss = criterion(outs[1:].view(-1, outs.shape[2]), trg[1:].view(-1))
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+        optimizer.step()
+        epoch_loss += loss.item()
+    return epoch_loss / len(iterator)
+
+
+def eval(model, iterator, criterion):
+    model.eval()
+    epoch_loss = 0
+
+    with torch.no_grad():
+        for batch in iterator:
+            src = batch.src
+            trg = batch.trg
+            outs = model(src, trg, None, 0)
+            loss = criterion(outs[1:].view(-1, outs.shape[2]),
+                             trg[1:].view(-1))
+            epoch_loss += loss.item()
+    return epoch_loss / len(iterator)
+
+
+if __name__ == '__main__':
+    main()
